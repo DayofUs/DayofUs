340fd984d51b71b55abecea87c261589ddac1c0d
DayofUs <zacdrummond17@gmail.com>
2026-08-08 16:48:53 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/dashboard', '/api/'],
     },
-    sitemap: 'https://dayofus.org/sitemap.xml',
+    sitemap: 'https://www.dayofus.org/sitemap.xml',
   }
 }
