240d41457dbfe8098b07d51ac3309c2f075b09de
DayofUs <zacdrummond17@gmail.com>
2026-08-08 16:47:03 +0800

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://dayofus.org'
+  const baseUrl = 'https://www.dayofus.org'
 
   return [
     { url: baseUrl, lastModified: new Date(), changeFrequency: 'weekly', priority: 1 },
